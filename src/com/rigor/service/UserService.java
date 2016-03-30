77a9102b066c423c2ca3c297b89bd7febcb5cb4a
nsranasinghe <radnsr@gmail.com>
2016-03-30 08:57:43 +0530

    UserImpl

---
+++ src/com/rigor/service/UserService.java
@@ -0,0 +1,27 @@
+package com.rigor.service;
+
+import java.util.List;
+
+import com.rigor.model.*;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
